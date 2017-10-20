942982435c99b512739d18446ca1b006653b4715
aktsbot <box.ashishk@gmail.com>
2017-10-20 12:26:06 +0530

    changed node 8 to 6

--- pi.sh
+++ pi.sh
@@ -32,7 +32,7 @@ installSublimeText() {
 
 installNodeJS() {
 	sudo apt-get install python-software-properties
-	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 	sudo apt-get install -y nodejs
 }
 
